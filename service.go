0d4f50a2bf67d2a11cff4e0395ccee47e1755b84
Michael Kennedy <michael@michaelkennedy.dev>
2025-08-03 16:58:37 -0700

    Consolodating code

--- service.go
+++ service.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
 	"log"
+	"net/http"
 	"os"
 	"path/filepath"
+	"regexp"
+	"strings"
+	"time"
 )
 
 func main() {
@@ -25,9 +33,9 @@ func main() {
 	}
 	
 	// Checking to make sure that someone isn't trying to pass malicious code in the ipGeoTz string
-	isValid, err := ValidateIanaName(ipGeoTz)
-
-	if !isValid {
+	if isValid, err := ValidateIanaName(ipGeoTz); err != nil {
+		log.Fatalf("Error validating IANA name: %v", err)
+	} else if !isValid {
 		log.Fatalf("Timezone is not valid: %s", ipGeoTz)
 	} else if (ipGeoTz != sysTz) {
 		timezonePath := filepath.Join(timezoneDir, ipGeoTz)
@@ -51,4 +59,72 @@ func main() {
 	} else {
 		log.Println("Time zone unchanged")
 	}
+}
+
+func ValidateIanaName(timezone string) (bool, error) {
+
+	validTimezoneRegex := `^[A-Za-z]+(/[A-Za-z_-]+)+$`
+    matched, err := regexp.MatchString(validTimezoneRegex, timezone)
+
+	if err != nil {
+        return false, fmt.Errorf("error compiling regex: %v", err)
+    }
+
+    if !matched {
+        return false, fmt.Errorf("invalid timezone format: %s", timezone)
+    }
+
+	return true, nil
+}
+
+func GetRemoteIanaName() (string, error) {
+	ipapiClient := http.Client{}
+
+	if req, err := http.NewRequest("GET", "http://ip-api.com/json/", nil); err != nil {
+		return "", err
+	} else {
+		resp, err := ipapiClient.Do(req)
+
+		if err != nil {
+			return "", err
+		}
+
+		defer resp.Body.Close()
+
+		body, err := io.ReadAll(resp.Body)
+
+		if err != nil {
+			return "", err
+		}
+		
+		var inputJson map[string]interface{}
+
+		json.Unmarshal([]byte(string(body)), &inputJson)
+		
+		return inputJson["timezone"].(string), nil
+	}
+}
+
+func GetLocalIanaName() (string, error) {
+    linkPath := "/etc/localtime"
+    targetPath, err := os.Readlink(linkPath)
+
+    if err != nil {
+        return "", err
+    }
+
+    tzParts := strings.Split(targetPath, "/")
+
+    if len(tzParts) < 3 {
+        return "", errors.New("invalid timezone format")
+    }
+
+    continent, country := tzParts[len(tzParts)-2], tzParts[len(tzParts)-1]
+    timezone := fmt.Sprintf("%s/%s", continent, country)
+
+    if _, err = time.LoadLocation(timezone); err != nil {
+		return "", err
+	}
+	
+    return timezone, nil
 }
